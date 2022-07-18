550d80d2a692b314dbacffbcc477764d29c0f548
nikolaysbv <89989774+nikolaysbv@users.noreply.github.com>
2022-07-18 20:50:08 +0300

    added logic for getting excel file and extracting data from it

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
+import { useState, useEffect } from "react";
 import "./App.css";
+import * as XLSX from "xlsx";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const handleChange = function (e) {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = (evt) => {
+      const binaryFile = evt.target.result;
+      const excelFileObj = XLSX.read(binaryFile, { type: "binary" });
+      const sheetName = excelFileObj.SheetNames[0];
+      const sheetData = excelFileObj.Sheets[sheetName];
+      const sheetDataJSON = XLSX.utils.sheet_to_json(sheetData, { header: 1 });
+      console.log(sheetDataJSON);
+      setData(sheetDataJSON);
+    };
+
+    reader.readAsBinaryString(file);
+  };
+
   return (
     <main>
-      <section className="upload-section">upload button</section>
+      <section className="upload-section">
+        <input type="file" id="sheet" onChange={handleChange} />
+      </section>
       <section className="table-section">
         <table>
           <thead>
             <tr>
-              <th colspan="4">Pair of employees who have worked together</th>
+              <th colSpan="4">Pair of employees who have worked together</th>
             </tr>
           </thead>
           <tbody>
             <tr>
-              <td>1</td>
-              <td>2</td>
-              <td>3</td>
-              <td>3</td>
+              <td>Employee ID #1</td>
+              <td>Employee ID #2</td>
+              <td>Project ID</td>
+              <td>Days worked</td>
             </tr>
+            {data.map((row, rowIndex) => {
+              return (
+                <tr key={rowIndex}>
+                  {row.map((cell, cellIndex) => (
+                    <td key={cellIndex}>{cell}</td>
+                  ))}
+                </tr>
+              );
+            })}
           </tbody>
         </table>
       </section>
