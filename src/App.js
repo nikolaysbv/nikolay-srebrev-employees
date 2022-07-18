c04429aa4d735384d62bcf0c94a8ac5582ccf5d8
nikolaysbv <89989774+nikolaysbv@users.noreply.github.com>
2022-07-18 19:56:27 +0300

    basic structure of page created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">test</div>;
+  return (
+    <main>
+      <section className="upload-section">upload button</section>
+      <section className="table-section">
+        <table>
+          <thead>
+            <tr>
+              <th colspan="4">Pair of employees who have worked together</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>2</td>
+              <td>3</td>
+              <td>3</td>
+            </tr>
+          </tbody>
+        </table>
+      </section>
+    </main>
+  );
 }
 
 export default App;
